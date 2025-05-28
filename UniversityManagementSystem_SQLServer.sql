2b715926da4a1fdc00ec60558e3a6e0084b3239c
Sabari Raj P <143380285+sabarirajpazhani@users.noreply.github.com>
2025-05-28 14:26:14 +0530

    Update UniversityManagementSystem_SQLServer.sql

--- UniversityManagementSystem_SQLServer.sql
+++ UniversityManagementSystem_SQLServer.sql
@@ -12,9 +12,28 @@ create table Students(
 	foreign key (DepartmentID) references Department(DepartmentID)
 );
 
+
 -- Creating Department table
 create table Department(
 	DepartmentID int primary key,
 	DepartmentName varchar(60)
+);
+
+
+--Creating Subjects table
+create table Subjects(
+	SubjectID int Primary key,
+	SubjectName varchar(50)
+);
+
+
+-- Creating SubjectMark table
+create table SubjectMark(
+	StudentID int,
+	SubjectID int,
+	Mark int,
+	primary key(StudentID,SubjectID),
+	foreign key(StudentID) references Students(StudentID),
+	foreign key(SubjectID) references Subjects(SubjectID)
 );
 
